b765b3c21b13858614e18661876735c57a0cefeb
davidsleeer <102831215@student.swin.edu.au>
2019-10-21 08:43:11 +1100

    Add Relational Schema

--- single.sql
+++ single.sql
@@ -1,3 +1,20 @@
 /*
 Dawei Li 102831215
 */
+
+/*
+
+Tour(TourName, Description)
+Primary Key TourName
+Event(EventYear, EventMonth, EventDay, Fee, TourName)
+Primary Key(EventYear, EventMonth, EventDay, TourName)
+Foreign Key (TourName) References Tour
+Booking(DateBooked, Payment, EventYear, EventMonth, EventDay, TourName, ClientID)
+Primary Key (EventYear, EventMonth, EventDay, TourName, ClientID)
+Foreign Key (EventYear, EventMonth, EventDay，TourName) References Event
+Foreign Key (ClientID) References Client
+Client(ClientID, Surname, GivenName, Gender)
+Primary Key (ClientID)
+
+*/
+
